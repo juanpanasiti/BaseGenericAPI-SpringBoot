e9dd4f6d780d5a6a42e141af1509fd1700c20246
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-04-13 19:56:10 -0300

    Iniciando proyecto de api genérica.

---
+++ src/main/java/com/giosoft/base/main/BaseGenericApiApplication.java
@@ -0,0 +1,13 @@
+package com.giosoft.base.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseGenericApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseGenericApiApplication.class, args);
+	}
+
+}
